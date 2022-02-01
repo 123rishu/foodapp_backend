46a25878c00b79959fdc9ae9ce5fe7026859a39b
Rishabh Sharma <sharmarishabh417@gmail.com>
2022-02-01 21:44:23 +0530

    authhelper bug solved

--- Routers/authHelper.js
+++ Routers/authHelper.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
-const {JWT_KEY} = require("../secrets2");
-console.log(JWT_KEY + "YO");
+require("dotenv").config();
+let JWT_KEY = process.env.JWT_KEY;
+
 function protectRoute(req, res, next){
     // console.log(req.cookies);
     try{
